1b69ed1a9792b6d3859046caa4bc70e4ba887214
qiaofeng1227 <76487013@qq.com>
2021-04-19 18:43:58 +0800

    Update zh.js

--- docs/.vuepress/sidebar/zh.js
+++ docs/.vuepress/sidebar/zh.js
@@ -32,7 +32,6 @@ module.exports = [
       '/zh/admin-services',
       ['https://support.websoft9.com/docs/linux/zh/webs-nginx.html','Nginx'],
       ['https://support.websoft9.com/docs/linux/zh/lang-Java.html','Java'],
-      ['https://support.websoft9.com/docs/docker/zh','Docker'],
       ['https://support.websoft9.com/docs/linux/zh/admin-practices.html','Linux'],
     ]
   },
